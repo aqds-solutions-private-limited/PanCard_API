f08077ca1ef875264d3b171256e11686b39529bf
ankie3596 <31041708+ankie3596@users.noreply.github.com>
2019-06-10 14:00:14 +0530

    Update retailer.aspx.cs

--- retailer.aspx.cs
+++ retailer.aspx.cs
@@ -20,15 +20,41 @@ namespace PenCardApi_Solutions
 
                 dt.Columns.AddRange(new DataColumn[3] 
                 {
-                    new DataColumn("Id", typeof(int)),
-                    new DataColumn("Name", typeof(string)),
-                    new DataColumn("Description",typeof(string)) });
-                dt.Rows.Add(1, "Twinkle", "Works as a scientist in USA.");
-                dt.Rows.Add(2, "Priyanka", "ASP.Net programmer and consultant in India.");
+                   // new DataColumn("Id", typeof(int)),
+                   // new DataColumn("Name", typeof(string)),
+                  //  new DataColumn("Description",typeof(string)) });
+               // dt.Rows.Add(1, "Twinkle", "Works as a scientist in USA.");
+                //dt.Rows.Add(2, "Priyanka", "ASP.Net programmer and consultant in India.");
 
+                //GridView1.DataSource = dt;
+                //GridView1.DataBind();
+            }
+            
+            
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+
+            con.Open();
+
+            cmd = new SqlCommand("ftch_byMemberType", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@member_type", "Retailer");
+
+
+
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            ad.Fill(dt);
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
                 GridView1.DataSource = dt;
                 GridView1.DataBind();
+
             }
+            else
+            { }
         }
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
         {
