6e5e47b107fc2eb38ae6a47af33fea80f75f1889
sunshinepinku <51503209+sunshinepinku@users.noreply.github.com>
2019-06-10 16:01:11 +0530

    webform2 updated

--- WebForm2.aspx.cs
+++ WebForm2.aspx.cs
@@ -11,34 +11,52 @@ namespace PenCardApi_Solutions
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
-        DataTable dt = new DataTable();
-        protected void Page_Load(object sender, EventArgs e)
-        {
+       DataTable dt = new DataTable();
+   // DataTable dt1 = new DataTable();
+    SqlConnection con = new SqlConnection("Data Source=intel-PC\\SQLEXPRESS;Initial Catalog=API_DB;Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //con.Open();
+        //string name = Session["username"].ToString();
+        //SqlCommand cmd1 = new SqlCommand("select * from add_member where mobile_no='" + name + "'", con);
+        //SqlDataAdapter ad1 = new SqlDataAdapter(cmd1);
+        //ad1.Fill(dt1);
+        //con.Close();
 
-            if (!this.IsPostBack)
-            {
+        //if (dt.Rows.Count > 0)
+        //{
 
-                dt.Columns.AddRange(new DataColumn[3] 
+            
+
+                con.Open();
+                if (Session["Member_Type"].ToString() == "Distributor")
                 {
-                    new DataColumn("Id", typeof(int)),
-                    new DataColumn("Name", typeof(string)),
-                    new DataColumn("Description",typeof(string))
-                });
-                dt.Rows.Add(1, "Twinkle", "Works as a scientist in USA.");
-                dt.Rows.Add(2, "Priyanka", "ASP.Net programmer and consultant in India.");
 
-                GridView1.DataSource = dt;
-                GridView1.DataBind();
-            }
-        }
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
+                    string get = "select mobile_no as [Agent id], name as [Agent Name], mobile_no as Mobile, state as State, email as [Email ID] from add_member where member_type='Retailer'";
+                    SqlCommand cmd = new SqlCommand(get, con);
+                    SqlDataAdapter ad = new SqlDataAdapter(cmd);
+                    ad.Fill(dt);
+                    con.Close();
+                    if (dt.Rows.Count > 0)
+                    {
 
-        }
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                    else
+                    {
 
-        protected void LinkButton1_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("WebForm8.aspx");
-        }
+
+                    }
+                }
+                else
+                    GridView1.Visible = false;
+        
+    }
+    
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
     }
 }
