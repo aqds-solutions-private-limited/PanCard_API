b8c7de5a000f9611f345b24a3f9f2db6847b8df0
sunshinepinku <51503209+sunshinepinku@users.noreply.github.com>
2019-06-10 17:59:47 +0530

    update

--- WebForm7.aspx.cs
+++ WebForm7.aspx.cs
@@ -10,13 +10,33 @@ namespace PenCardApi_Solutions
     public partial class WebForm7 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        
+    DataTable dt = new DataTable();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!this.IsPostBack)
         {
 
-        }
+            dt.Columns.AddRange(new DataColumn[3] 
+                {
+                    new DataColumn("Id", typeof(int)),
+                    new DataColumn("Name", typeof(string)),
+                    new DataColumn("Description",typeof(string)) });
+            dt.Rows.Add(1, "Twinkle", "Works as a scientist in USA.");
+            dt.Rows.Add(2, "Priyanka", "ASP.Net programmer and consultant in India.");
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
         }
+               
+            }
+
+
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
     }
 }
