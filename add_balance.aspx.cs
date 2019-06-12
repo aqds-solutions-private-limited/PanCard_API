9a5c94f5c2ea4db488c0a410b2bdf497206e5c3f
ankie3596 <31041708+ankie3596@users.noreply.github.com>
2019-06-12 15:17:34 +0530

    Update add_balance.aspx.cs

--- add_balance.aspx.cs
+++ add_balance.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,55 +7,137 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
-public partial class add_balance : System.Web.UI.Page
+
+namespace PenCardApi_Solutions
 {
-    string id;
-    DataTable dt = new DataTable();
-    SqlConnection con = new SqlConnection("Data Source=intel-PC\\SQLEXPRESS;Initial Catalog=API_DB;Integrated Security=True");
-    SqlCommand cmd = new SqlCommand();
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class add_balance : System.Web.UI.Page
     {
-        id = Request.QueryString["Agent id"].ToString();
-        if (!Page.IsPostBack)
-        {
-            getdata();
-        }
+        string id;
+        DataTable dt = new DataTable();
+        DataTable dt1 = new DataTable();
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-4NFLJ61\\SQLEXPRESS;Initial Catalog=API_DB;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
 
-    }
-    public void getdata()
-    {
-
-        dt.Clear();
-        dt.Rows.Clear();
-        if (con.State == ConnectionState.Open)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            con.Close();
+            id = Request.QueryString["Agent id"].ToString();
+            if (!Page.IsPostBack)
+            {
+                getdata();
+            }
         }
-        con.Open();
+        string id1;
        
-        SqlCommand cmd = new SqlCommand("select * from add_member where mobile_no='" + id + "'", con);
-        SqlDataAdapter ad = new SqlDataAdapter(cmd);
-        ad.Fill(dt);
-        con.Close();
-        if (dt.Rows.Count > 0)
+        public void getdata()
         {
-          //  Label1.Text = dt.Rows[0]["Current Balance"].ToString();
-            Label2.Text = dt.Rows[0]["Name"].ToString();
-           Label3.Text = dt.Rows[0]["Postal_Address"].ToString();
-             Label5.Text = dt.Rows[0]["mobile_no"].ToString();
-            Label6.Text = dt.Rows[0]["Email"].ToString();
-            //Label7.Text = dt.Rows[0]["Add Amount"].ToString();
-            //Label8.Text = dt.Rows[0]["Remark"].ToString();
+            
+            dt.Clear();
+            dt.Rows.Clear();
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from add_member where mobile_no='" + id + "'", con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            ad.Fill(dt);
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
+                id1 = dt.Rows[0]["Id"].ToString();
+                //  Label1.Text = dt.Rows[0]["Current Balance"].ToString();
+                Label2.Text = dt.Rows[0]["Name"].ToString();
+                Label3.Text = dt.Rows[0]["address"].ToString();
+                Label5.Text = dt.Rows[0]["mobile_no"].ToString();
+                Label6.Text = dt.Rows[0]["Email"].ToString();
+          
+                //Label7.Text = dt.Rows[0]["Add Amount"].ToString();
+                //Label8.Text = dt.Rows[0]["Remark"].ToString();
+
+            }
+        }
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Panel_otp.Visible = true;
+            
 
         }
 
-    }
-    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
-    {
+        string otp_num;
+        public void getdata_Login()
+        {
+            try
+            {
 
-    }
-    protected void Button1_Click(object sender, EventArgs e)
-    {
+                dt1.Clear();
+                dt1.Rows.Clear();
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                con.Open();
 
+                SqlCommand cmd1 = new SqlCommand("select * from login_table where username='" + Session["username"].ToString() + "'", con);
+                SqlDataAdapter ad1 = new SqlDataAdapter(cmd1);
+                ad1.Fill(dt1);
+                con.Close();
+                if (dt1.Rows.Count > 0)
+                {
+                  //  id1 = dt.Rows[0]["Id"].ToString();
+                    otp_num = dt1.Rows[0]["otp"].ToString();
+
+                }
+            }
+            catch (Exception e)
+            { }
+        }
+        protected void Proceed_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                getdata_Login();
+                if (otp_num == TextBox3.Text)
+                {
+
+                    if (con.State == ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+
+                    con.Open();
+                    cmd = new SqlCommand("add_balance", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@username", Label5.Text);
+
+                    cmd.Parameters.AddWithValue("@wallet", TextBox2.Text);
+                    cmd.Parameters.AddWithValue("@remarks", TextBox1.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Hurray!', 'Payment is transferred successfully!', 'success');", true);
+                }
+                else
+                {
+                    Label7.Text= "Invalid OTP";
+                }
+            }
+            catch (Exception ex)
+            {
+                 
+            }
+        }
+    
+
+        protected void Close_Click(object sender, EventArgs e)
+        {
+            Panel_otp.Visible = false;
+        }
+        
     }
 }
