9c9e3b1453b99ad23167f8389cb30231d3dbcffc
twinklemulwani <51503231+twinklemulwani@users.noreply.github.com>
2019-06-12 11:53:04 +0530

    complaint cs

---
+++ Complaint.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+public partial class Complaint : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+     string name = Session["username"].ToString();
+        SqlConnection con = new SqlConnection("Data Source=Abc-PC\\SQLEXPRESS;Initial Catalog=API-DB;Integrated Security=True;Pooling=False");
+        SqlCommand cmd = new SqlCommand();
+ 
+    con.Open();
+   
+      
+
+
+        cmd = new SqlCommand("StoredProcedure5", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+    
+    cmd.ExecuteNonQuery();
+   con.Close();
+
+    }
+}
